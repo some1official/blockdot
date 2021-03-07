c54bf780ae38d591947138872c34808fbc3d5389
timnik <72356939+nottimnik@users.noreply.github.com>
2021-03-07 23:57:48 +0200

    Initial Commit

---
+++ hash/alphabet.py
@@ -0,0 +1,76 @@
+#Here are all the letters and characters that can be translated in BlockHash
+#To all the characters are assigned a id
+
+#English Alphabet Characters downcase
+a = 0 #hjb
+b = 1 #dsv
+c = 2 #b1d
+d = 3 #ewv
+e = 4 #ffd
+f = 5 #wev
+g = 6 #b2b
+h = 7 #43b
+i = 8 #2bf
+j = 9 #bg8
+k = 10 #bfd
+l = 11 #bd9
+m = 12 #fb2
+n = 13 #b10
+o = 14 #as1
+p = 15 #jf4
+q = 16 #ek2
+r = 17 #mv3
+s = 18 # bk9
+t = 19 #kbf
+u = 20 #jsm
+v = 21 #ibm
+w = 22 #iem
+x = 23 #mve
+y = 24 #im3
+z = 25 #vm1
+
+#English Alphabet Characters uppercase
+A = 26 #843
+B = 27 #nb3
+C = 28 #dm2
+D = 29 #mb3
+E = 30 #mb8
+F = 31 #vda
+G = 32 #mkb
+H = 33 #mus
+I = 34 #mb6
+J = 35 #65m
+K = 36 #mbd
+L = 37 #nbs
+M = 38 #sfa
+N = 39 #dpa
+O = 40 #mid
+P = 41 #mid
+Q = 42 #8gm
+R = 43 #jb7
+S = 44 #yus
+T = 45 #m02
+U = 46 #abd
+V = 47 #7a3
+W = 48 #75a
+X = 49 #92r
+Y = 50 #m8a
+Z = 51 #mao
+
+#Digits Characters
+zero = 52 #kms
+one = 53 #lks
+two = 54 #vhd
+three = 55 #meb
+four = 56 #m6m
+five = 57 #8mv
+six = 58 #mk3
+seven = 59 #84j
+eight = 60 #yfs
+nine = 61 #kms
+
+#Other Characters
+question_mark = 63 #m8s
+space = 64 #mav
+exclamation_mark = 65 #lsa
+dot = 66 #m8a
---
+++ hash/translate.py
@@ -0,0 +1,139 @@
+from alphabet import *
+
+#translates characters to id
+def translate_to_id(char):
+    char = str(char)
+    if(char=="a"):
+        return a
+    elif(char=="b"):
+        return b
+    elif(char=="c"):
+        return c
+    elif(char=="d"):
+        return d
+    elif(char=="e"):
+        return e
+    elif(char=="f"):
+        return f
+    elif(char=="g"):
+        return g
+    elif(char=="h"):
+        return h
+    elif(char=="i"):
+        return i
+    elif(char=="j"):
+        return j
+    elif(char=="k"):
+        return k
+    elif(char=="l"):
+        return l
+    elif(char=="m"):
+        return m
+    elif(char=="n"):
+        return n
+    elif(char=="o"):
+        return o
+    elif(char=="p"):
+        return p
+    elif(char=="q"):
+        return q
+    elif(char=="r"):
+        return r
+    elif(char=="s"):
+        return s
+    elif(char=="t"):
+        return t
+    elif(char=="u"):
+        return u
+    elif(char=="v"):
+        return v
+    elif(char=="w"):
+        return w
+    elif(char=="x"):
+        return x
+    elif(char=="y"):
+        return y
+    elif(char=="z"):
+        return z
+    elif(char=="A"):
+        return A
+    elif(char=="B"):
+        return B
+    elif(char=="C"):
+        return C
+    elif(char=="D"):
+        return D
+    elif(char=="E"):
+        return E
+    elif(char=="F"):
+        return F
+    elif(char=="G"):
+        return G
+    elif(char=="H"):
+        return H
+    elif(char=="I"):
+        return I
+    elif(char=="J"):
+        return J
+    elif(char=="K"):
+        return K
+    elif(char=="L"):
+        return L
+    elif(char=="M"):
+        return M
+    elif(char=="N"):
+        return N
+    elif(char=="O"):
+        return O
+    elif(char=="P"):
+        return P
+    elif(char=="Q"):
+        return Q
+    elif(char=="R"):
+        return R
+    elif(char=="S"):
+        return S
+    elif(char=="T"):
+        return T
+    elif(char=="U"):
+        return U
+    elif(char=="V"):
+        return V
+    elif(char=="W"):
+        return W
+    elif(char=="X"):
+        return X
+    elif(char=="Y"):
+        return Y
+    elif(char=="Z"):
+        return Z
+    elif(char=="0"):
+        return zero
+    elif(char=="1"):
+        return one
+    elif(char=="2"):
+        return two
+    elif(char=="3"):
+        return three
+    elif(char=="4"):
+        return four
+    elif(char=="5"):
+        return five
+    elif(char=="6"):
+        return six
+    elif(char=="7"):
+        return seven
+    elif(char=="8"):
+        return eight
+    elif(char=="9"):
+        return nine
+    elif(char=="?"):
+        return question_mark
+    elif(char==" "):
+        return space
+    elif(char=="!"):
+        return exclamation_mark
+    elif(char=="."):
+        return dot
+    else:
+        return None
