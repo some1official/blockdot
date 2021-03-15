51ff0f03bacbd608789457ade13cccd59428da78
timnik <72356939+nottimnik@users.noreply.github.com>
2021-03-15 22:57:07 +0100

    Moved alphabet from Hash

---
+++ src/alphabet.py
@@ -0,0 +1,76 @@
+#Here are all the letters and characters that can be translated in BlockHash
+#To all the characters are assigned a id
+
+#English Alphabet Characters downcase
+a = 0
+b = 1
+c = 2
+d = 3
+e = 4
+f = 5
+g = 6
+h = 7
+i = 8
+j = 9
+k = 10
+l = 11
+m = 12
+n = 13
+o = 14
+p = 15
+q = 16
+r = 17
+s = 18
+t = 19
+u = 20
+v = 21
+w = 22
+x = 23
+y = 24
+z = 25
+
+#English Alphabet Characters uppercase
+A = 26
+B = 27
+C = 28
+D = 29
+E = 30
+F = 31
+G = 32
+H = 33
+I = 34
+J = 35
+K = 36
+L = 37
+M = 38
+N = 39
+O = 40
+P = 41
+Q = 42
+R = 43
+S = 44
+T = 45
+U = 46
+V = 47
+W = 48
+X = 49
+Y = 50
+Z = 51
+
+#Digits Characters
+zero = 52
+one = 53
+two = 54
+three = 55
+four = 56
+five = 57
+six = 58
+seven = 59
+eight = 60
+nine = 61
+
+#Other Characters
+question_mark = 62
+space = 63
+exclamation_mark = 64
+dot = 65
